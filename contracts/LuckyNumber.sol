5661dfd459498aa258a21249b67707290e237d1a
Carlos Oporto <carlosoporto@gmail.com>
2019-04-03 22:31:18 +0200

    contract selfdestoy by owner

--- contracts/LuckyNumber.sol
+++ contracts/LuckyNumber.sol
@@ -1,5 +1,25 @@
 // Solidity version
-pragma ^solidity 0.5.7;
+pragma solidity ^0.5.7;
+
+// Contracts
+contract Owned {
+  address payable owner;
+
+  constructor() public {
+    // Sets the owner of Contract
+    owner = msg.sender;
+  }
+}
+
+contract Mortal is Owned {
+  // Our access modifier is present, only the contract creator can use this function
+  function kill() public {
+  if (msg.sender == owner) selfdestruct(owner);
+  }
+}
+
+
+// Imports
 
 // Contract variables
 
