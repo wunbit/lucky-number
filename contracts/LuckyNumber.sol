5ecc50c59b657add1c98aed9d1b0c79ee7ae5e57
Carlos Oporto <carlosoporto@gmail.com>
2019-04-04 16:35:04 +0200

    create constructor and fallback function in Lucky Number contract

--- contracts/LuckyNumber.sol
+++ contracts/LuckyNumber.sol
@@ -18,6 +18,26 @@ contract Mortal is Owned {
   }
 }
 
+contract LuckyNumber is Mortal {
+  uint minBet;
+  uint houseEdge; // in %
+
+  //true+amount or false+0
+  event Won(bool _status, uint _amount);
+
+  constructor(uint _minBet, uint _houseEdge) payable public {
+    require(_minBet > 0);
+    require(_houseEdge <= 100);
+    minBet = _minBet;
+    houseEdge = _houseEdge;
+  }
+
+  // Fallback function
+  function() external {
+    revert();
+  }
+}
+
 
 // Imports
 
